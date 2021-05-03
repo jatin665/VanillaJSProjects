d5fc35bc62cbb6715fa2d5cd547c39bd4c559661
jatin bhatia <jatinb@azuga.com>
2021-05-03 23:50:00 +0530

    Responsive Navbar with Animation

---
+++ responsive-side-nav/app.js
@@ -0,0 +1,22 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li')
+
+// Event Listeners
+burger.addEventListener('click', function() {
+    // Side-panel toggling
+    nav.classList.toggle('nav-active');
+
+    // Burger <-> Close Animation
+    this.classList.toggle('toggle');
+
+    // Nav Links Animation
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            // Removing animation when closing side nav
+            link.style.animation = '';
+        } else {
+            link.style.animation = `slideIn 0.5s ease forwards ${index / 7 + 0.5}s`;
+        }
+    })
+});
